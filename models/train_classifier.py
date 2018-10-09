7373e9964af9cd8348426494fd89678292d85bc4
Cady Patino <cnspatino@gmail.com>
2018-10-09 18:12:17 -0500

    Updated train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,111 @@
 import sys
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+
+import pandas as pd
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import re
+import pickle
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report, accuracy_score
 
 
 def load_data(database_filepath):
-    pass
+    """
+    This function loads data by reading a sql table from the specified database
+    and then splits the data into features and target.
+
+    Input: database_filepath (string)
+    Outputs: X (features array), Y (target variables array)
+    """
+    engine = create_engine(database_filepath)
+    df = read_sql_table('MessageClassification', engine)
+
+    # designate features array and target array
+    X = df['message'].values
+    Y = df.drop(['id','message','original','genre'], axis=1).values
+
+    return X, Y
 
 
 def tokenize(text):
-    pass
+    """
+    This function normalizes, lemmatizes, and tokenizes text.
+    """
+    # replace non-letter and non-number characters with a space, convert all to lowercase
+    text = re.sub(r'[^a-zA-z0-9]', ' ', text.lower())
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # Initialize lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    
+    # lemmatize the tokens
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).strip()
+        clean_tokens.append(clean_tok)
+    
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """
+    This function builds a model using a machine learning pipeline and returns
+    a gridsearch object ready to be trained on the training set.
+
+    Input: None
+    Output: cv (grid search object)
+    """
+    # build pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfid', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    # specify parameters for grid search
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'clf__estimator__n_estimators': [5,10,20],
+    }
+    # create grid search object
+    cv = GridSearchCV(pipeline, param_grid = parameters, verbose=2)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    This function evaluates the given model by predicting Y values from the test set
+    and prints out the accuracy score and classification report for each Y column.
+    """
+    # predict Y values given X_test using model
+    Y_preds = model.predict(X_test)
+
+    # convert Y arrays to dataframes
+    Y_preds = pd.DataFrame(Y_preds, columns = categories_names)
+    Y_test = pd.DataFrame(Y_test, columns = categories_names)
+
+    for column in Y_preds:
+        accuracy = accuracy_score(Y_test[column], Y_preds[column])
+        report = classification_report(Y_test[column], Y_preds[column])
+        print(column+':')
+        print(report)
+        print('Accuracy: {:.4f}'.format(accuracy),'\n')
 
 
 def save_model(model, model_filepath):
-    pass
+    # save the model to disk
+    filename = model_filepath
+    pickle.dump(model, open(filename, 'wb'))
 
 
 def main():
